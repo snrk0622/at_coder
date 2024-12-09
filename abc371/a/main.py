4817cf8a7be73eee1f15446274c054b284416475
snrk0622 <snrk.0622@gmail.com>
2024-12-09 23:54:49 +0900

    abc371_a_修正

--- abc371/a/main.py
+++ abc371/a/main.py
@@ -1,17 +1,8 @@
-# 年齢順を表すリスト（初期値[a, b, c]）を用意する
-# それぞれの入力値を元に上記リストを並び替える
+AB, AC, BC = list(input().split())
 
-S = list(input().split())
-ans = ['A', 'B', 'C']
-
-for i in range(len(S)):
-  if i == 0: # ab
-    if S[i] == '>':
-      ans[0], ans[1] = ans[1], ans[0]
-  elif i == 1: # ac
-    if S[i] == '>':
-      ans[0], ans[2] = ans[2], ans[0]
-  else: #bc
-    if S[i] == '>':
-      ans[1], ans[2] = ans[2], ans[1]
-print(ans[1])
+if AB == AC == BC:
+  print('B')
+elif AB == AC:
+  print('C')
+else:
+  print('A')
