9186824731628a764b04ace57631fbf57d150c19
snrk0622 <snrk.0622@gmail.com>
2025-01-22 21:23:43 +0900

    wip

--- abs/abc049c/answer.py
+++ abs/abc049c/answer.py
@@ -1,1 +1,25 @@
-# wip
+# 前から読むと
+# dreamとdreamer
+# eraseとeraserが被ってしまい判定が複雑になる
+
+# 後ろから読むと被ることがなくなる！！！
+# 後ろから考えると難易度が下がる問題は結構出る
+
+S = input()
+reversedS = S[::-1]
+
+keys = ['eraser', 'erase', 'dreamer', 'dream']
+reversedKeys = list(map(lambda x: x[::-1], keys))
+
+can = False
+for i in range(len(S)):
+  can2 = False # keysのいずれかでdevideできるか
+  for key in reversedKeys:
+    if reversedS[i:i+len(key)+1] == key:
+      can2 = True
+      i += len(key)
+  if not can2:
+    can = False
+    break
+
+print('YES' if can else 'NO')
