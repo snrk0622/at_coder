55d28556f9f739e96ab9c562a349933d47c0418f
snrk0622 <snrk.0622@gmail.com>
2025-02-04 15:18:18 +0900

    mat_013_review

---
+++ math-and-algorithm/013/main_2.py
@@ -0,0 +1,10 @@
+import math
+N = int(input())
+divisors = []
+for i in range(1, math.floor(math.sqrt(N))+1):
+  if N % i == 0:
+    divisors.append(i)
+    divisors.append(N // i)
+
+for divisor in divisors:
+  print(divisor)
