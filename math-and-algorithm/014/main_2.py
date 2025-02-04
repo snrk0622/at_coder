935e809e191fbd5c808611c4e3b6d8fa2d244645
snrk0622 <snrk.0622@gmail.com>
2025-02-04 15:22:31 +0900

    mat_014_review

---
+++ math-and-algorithm/014/main_2.py
@@ -0,0 +1,13 @@
+import math
+N = int(input())
+factors = []
+while True:
+  for i in range(2, math.floor(math.sqrt(N))+1):
+    if N % i == 0:
+      factors.append(i)
+      N = N // i
+      break
+  else:
+    factors.append(N)
+    break
+print(*factors)
