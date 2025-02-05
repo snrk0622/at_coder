966d51b2b458e11fac79a0adf42c73572b6249bb
snrk0622 <snrk.0622@gmail.com>
2025-02-06 08:31:11 +0900

    mat_016_review

---
+++ math-and-algorithm/016/main_2.py
@@ -0,0 +1,12 @@
+from collections import deque
+N = int(input())
+nums = deque(map(int, input().split()))
+
+A = nums.popleft()
+while len(nums):
+  B = nums.popleft()
+  divisor, divided = sorted([A, B])
+  while divisor:
+    divisor, divided = divided % divisor, divisor
+  A = divided
+print(A)
